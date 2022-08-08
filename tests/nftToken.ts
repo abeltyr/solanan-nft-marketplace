5013ed1f31a78ad117f968997f8b73a592088aae
abeltyr <abeltyr@gmail.com>
2022-08-08 21:31:00 +0300

    using spl-token packages as alternative for the rust contract

---
+++ tests/nftToken.ts
@@ -0,0 +1,144 @@
+import {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  Transaction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import {
+  createMint,
+  getMint,
+  getOrCreateAssociatedTokenAccount,
+  getAccount,
+  mintTo,
+} from "@solana/spl-token";
+import {
+  findMasterEditionV2Pda,
+  findMetadataPda,
+} from "@metaplex-foundation/js";
+import * as mpl from "@metaplex-foundation/mpl-token-metadata";
+
+describe("Mint the Nft", async () => {
+  let connection;
+  let payer;
+
+  it("Setup An account", async () => {
+    connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+    let accountKey = require("./keypairs/first.json");
+
+    const secretKey = Uint8Array.from(accountKey);
+    payer = Keypair.fromSecretKey(secretKey);
+
+    // await connection.requestAirdrop(payer.publicKey, LAMPORTS_PER_SOL);
+  });
+
+  it("Mint Nft!", async () => {
+    const mint = await createMint(
+      connection,
+      payer,
+      payer.publicKey,
+      payer.publicKey,
+      0, // We are using 0 to match the CLI decimal default exactly
+    );
+
+    console.log("The Mint Account", mint?.toBase58());
+
+    const mintInfo = await getMint(connection, mint);
+
+    console.log("MINT INFO", mintInfo.supply);
+
+    const tokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      payer,
+      mint,
+      payer.publicKey,
+    );
+
+    console.log(tokenAccount.address.toBase58());
+
+    const tokenAccountInfo = await getAccount(connection, tokenAccount.address);
+
+    console.log(tokenAccountInfo.amount);
+
+    await mintTo(
+      connection,
+      payer,
+      mint,
+      tokenAccount.address,
+      payer,
+      1, // because decimals for the mint are set to 0
+    );
+
+    // Set up the metadata using metaplex
+    const ON_CHAIN_METADATA = {
+      name: "MeMusicNFT",
+      symbol: "MNFT",
+      uri: "TO_UPDATE_LATER",
+      sellerFeeBasisPoints: 0,
+      creators: null,
+      collection: null,
+      uses: null,
+    } as mpl.DataV2;
+
+    //metadata account associated with mint
+    const metadataPDA = await findMetadataPda(mint);
+
+    console.log("Metadata initialized", metadataPDA.toString());
+
+    let metadataTransaction = new Transaction().add(
+      mpl.createCreateMetadataAccountV2Instruction(
+        {
+          metadata: metadataPDA,
+          mint: mint,
+          mintAuthority: payer.publicKey,
+          updateAuthority: payer.publicKey,
+          payer: payer.publicKey,
+        },
+        {
+          createMetadataAccountArgsV2: {
+            isMutable: false,
+            data: ON_CHAIN_METADATA,
+          },
+        },
+      ),
+    );
+
+    await sendAndConfirmTransaction(connection, metadataTransaction, [payer]);
+
+    const masterEditionPda = await findMasterEditionV2Pda(mint);
+    let masterTransaction = new Transaction().add(
+      mpl.createCreateMasterEditionV3Instruction(
+        {
+          edition: masterEditionPda,
+          metadata: metadataPDA,
+          mint: mint,
+          mintAuthority: payer.publicKey,
+          updateAuthority: payer.publicKey,
+          payer: payer.publicKey,
+        },
+        {
+          createMasterEditionArgs: {
+            maxSupply: 0,
+          },
+        },
+      ),
+    );
+
+    await sendAndConfirmTransaction(connection, masterTransaction, [payer]);
+
+    // The metaplex metadata make max supply to to limit the supply if that is not set
+    // an Alternative would be freezing the mint after the first mint
+
+    // let transaction = new Transaction().add(
+    //   createSetAuthorityInstruction(
+    //     mint,
+    //     payer.publicKey,
+    //     AuthorityType.MintTokens,
+    //     null,
+    //   ),
+    // );
+
+    // await sendAndConfirmTransaction(connection, transaction, [payer]);
+  });
+});
