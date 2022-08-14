8aea925dd2c8e2012e96e710b498e2a031802f41
abeltyr <abeltyr@gmail.com>
2022-08-14 14:18:56 +0300

    update to the buy function setup

--- programs/nft/src/processor/buy_nft_fixed_price_listing.rs
+++ programs/nft/src/processor/buy_nft_fixed_price_listing.rs
@@ -39,76 +39,76 @@ pub fn buy_nft_fixed_price_listing_fn(ctx: Context<BuyNftFixedPriceListing>) -> 
         return Err(ErrorCode::ListingNotActivate.into());
     }
 
-    // transfer the fund to the account
+    // // transfer the fund to the account
 
-    system_program::transfer(
-        CpiContext::new(
-            ctx.accounts.system_program.to_account_info(),
-            system_program::Transfer {
-                from: ctx.accounts.buyer.to_account_info(),
-                to: ctx.accounts.program_account.to_account_info(),
-            },
-        ),
-        listing_account.price_lamports,
-    )?;
+    // system_program::transfer(
+    //     CpiContext::new(
+    //         ctx.accounts.system_program.to_account_info(),
+    //         system_program::Transfer {
+    //             from: ctx.accounts.buyer.to_account_info(),
+    //             to: ctx.accounts.program_account.to_account_info(),
+    //         },
+    //     ),
+    //     listing_account.price_lamports,
+    // )?;
 
-    listing_account.fund_deposited = Some(true);
-    listing_account.buyer = Some(ctx.accounts.buyer.key());
+    // listing_account.fund_deposited = Some(true);
+    // listing_account.buyer = Some(ctx.accounts.buyer.key());
 
-    // transfer the NFT To buyer
+    // // transfer the NFT To buyer
 
-    let seller_token_account = associated_token::get_associated_token_address(
-        &listing_account.seller.key(),
-        &ctx.accounts.mint.key(),
-    );
+    // let seller_token_account = associated_token::get_associated_token_address(
+    //     &listing_account.seller.key(),
+    //     &ctx.accounts.mint.key(),
+    // );
 
-    let buyer_token_account = associated_token::get_associated_token_address(
-        &ctx.accounts.buyer.key(),
-        &ctx.accounts.mint.key(),
-    );
+    // let buyer_token_account = associated_token::get_associated_token_address(
+    //     &ctx.accounts.buyer.key(),
+    //     &ctx.accounts.mint.key(),
+    // );
 
-    if seller_token_account.key() != ctx.accounts.seller_token_account.key() {
-        return Err(ErrorCode::InvalidTokenAccount.into());
-    }
+    // if seller_token_account.key() != ctx.accounts.seller_token_account.key() {
+    //     return Err(ErrorCode::InvalidTokenAccount.into());
+    // }
 
-    if buyer_token_account.key() != ctx.accounts.buyer_token_account.key() {
-        return Err(ErrorCode::InvalidTokenAccount.into());
-    }
+    // if buyer_token_account.key() != ctx.accounts.buyer_token_account.key() {
+    //     return Err(ErrorCode::InvalidTokenAccount.into());
+    // }
 
-    token::transfer(
-        CpiContext::new(
-            ctx.accounts.token_program.to_account_info(),
-            token::Transfer {
-                from: ctx.accounts.seller_token_account.to_account_info(),
-                to: ctx.accounts.buyer_token_account.to_account_info(),
-                authority: ctx.accounts.program_account.to_account_info(),
-            },
-        ),
-        1,
-    )?;
+    // token::transfer(
+    //     CpiContext::new(
+    //         ctx.accounts.token_program.to_account_info(),
+    //         token::Transfer {
+    //             from: ctx.accounts.seller_token_account.to_account_info(),
+    //             to: ctx.accounts.buyer_token_account.to_account_info(),
+    //             authority: ctx.accounts.program_account.to_account_info(),
+    //         },
+    //     ),
+    //     1,
+    // )?;
 
-    // transfer the fund to the seller
-    system_program::transfer(
-        CpiContext::new(
-            ctx.accounts.system_program.to_account_info(),
-            system_program::Transfer {
-                from: ctx.accounts.program_account.to_account_info(),
-                to: ctx.accounts.seller.to_account_info(),
-            },
-        ),
-        listing_account.price_lamports,
-    )?;
+    // // transfer the fund to the seller
+    // system_program::transfer(
+    //     CpiContext::new(
+    //         ctx.accounts.system_program.to_account_info(),
+    //         system_program::Transfer {
+    //             from: ctx.accounts.program_account.to_account_info(),
+    //             to: ctx.accounts.seller.to_account_info(),
+    //         },
+    //     ),
+    //     listing_account.price_lamports,
+    // )?;
 
-    // update the nft listing pda
-    nft_listing_account.active = false;
+    // // update the nft listing pda
+    // nft_listing_account.active = false;
 
-    // close the fixed price listing pda
-    let close_date: u64 = Clock::get().unwrap().unix_timestamp as u64;
-    listing_account.close_date = Some(close_date);
-    listing_account.sold = Some(true);
-    listing_account.is_active = false;
-    listing_account.buyer_token = Some(buyer_token_account.key());
-    listing_account.fund_withdrawn = Some(true);
+    // // close the fixed price listing pda
+    // let close_date: u64 = Clock::get().unwrap().unix_timestamp as u64;
+    // listing_account.close_date = Some(close_date);
+    // listing_account.sold = Some(true);
+    // listing_account.is_active = false;
+    // listing_account.buyer_token = Some(buyer_token_account.key());
+    // listing_account.fund_withdrawn = Some(true);
 
     Ok(())
 }
